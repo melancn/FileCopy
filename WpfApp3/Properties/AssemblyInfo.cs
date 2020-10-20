741a8215d4084c08705caaebc0607edc048dfc9a
melancn <cc@cnuer.cn>
2020-10-20 16:34:24 +0800

    更新文件

--- WpfApp3/Properties/AssemblyInfo.cs
+++ WpfApp3/Properties/AssemblyInfo.cs
@@ -10,10 +10,10 @@ using System.Windows;
 [assembly: AssemblyTitle("FileCopy")]
 [assembly: AssemblyDescription("代码文件拷贝")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ysbang")]
+[assembly: AssemblyCompany("melan")]
 [assembly: AssemblyProduct("FileCopy By PHP")]
-[assembly: AssemblyCopyright("Copyright © ysbang 2020")]
-[assembly: AssemblyTrademark("ysbang")]
+[assembly: AssemblyCopyright("Copyright © melan 2020")]
+[assembly: AssemblyTrademark("melan")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
